a0ce1cc8577ae296893d640f614860fc6fa1492c
raghavaspraghava-dot <raghavaspraghava@gmail.com>
2026-02-21 00:47:13 +0530

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client
-from config import SUPABASE_URL, SUPABASE_KEY
-
-try:
-    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-    print("✅ Supabase connected")
-except:
-    supabase = None
-    print("⚠️ Supabase failed")
